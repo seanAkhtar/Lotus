22471fbedc60ec46b8d89c461aeca8803ca5aad5
Sean <usakhtar@pm.me>
2021-09-06 22:06:12 -0500

    created my own solar system

--- buddAR/ContentView.swift
+++ buddAR/ContentView.swift
@@ -21,6 +21,7 @@ struct ARViewContainer: UIViewRepresentable {
         let arView = ARView(frame: .zero)
         
         // Load the "Box" scene from the "Experience" Reality File
+        
         let boxAnchor = try! Experience.loadBox()
         
         // Add the box anchor to the scene
